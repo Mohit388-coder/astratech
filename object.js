66177488a4384184f8fbddda058003bf9c444e2a
Mohit388-coder <mohitrajnewmail@gmail.com>
2026-05-29 09:59:38 +0530

    commit

--- object.js
+++ object.js
@@ -3,7 +3,7 @@ const student = {
     class: 12,
     rollno: 23,
     marks: {eng: 90, math: 95, sci: 85},
-    points: [10, 20, 30, 40, 50],
+    points: [10, 20, 30, 40, 50,{a:80}],
     age: 80,
     
 }
@@ -13,3 +13,4 @@ console.log(student.marks.eng);
 console.log(student.name);
 console.log(student.age);
 console.log(student);
+console.log(student.points[5].a);
