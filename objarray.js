a1b91073bce198403f181adf6131fe823dd42af9
Mohit388-coder <mohitrajnewmail@gmail.com>
2026-05-29 10:12:13 +0530

    new commit

---
+++ objarray.js
@@ -0,0 +1,16 @@
+const data = [
+  {
+    id: "a",
+    news: "Pak cannot be found since 2 days"
+  },
+
+  {
+    id: "b",
+    news: "Charan Kanya found on North Korean border"
+  },
+
+  {
+    id: "c",
+    news: "From Monday Nepal will be a state of India"
+  }
+];
