f6111cc63c90c914141c2b46ddde117a2361a504
Mohit388-coder <mohitrajnewmail@gmail.com>
2026-05-29 09:56:06 +0530

    new commit

---
+++ object.js
@@ -0,0 +1,15 @@
+const student = {
+    name: "mohit",
+    class: 12,
+    rollno: 23,
+    marks: {eng: 90, math: 95, sci: 85},
+    points: [10, 20, 30, 40, 50],
+    age: 80,
+    
+}
+
+console.log(student.points[2]);
+console.log(student.marks.eng);
+console.log(student.name);
+console.log(student.age);
+console.log(student);
