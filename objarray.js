d4f3996a8ce86a92514f431e83a85e20bffe0159
Mohit388-coder <mohitrajnewmail@gmail.com>
2026-05-29 10:40:42 +0530

    commit

--- objarray.js
+++ objarray.js
@@ -14,3 +14,21 @@ const data = [
     news: "From Monday Nepal will be a state of India"
   }
 ];
+console.log(data[0].news);
+console.log(data[1]);
+
+
+function findbyid(id){
+    for(let i =0;i<data.length;i++){
+        if(data[i].id === id){
+            return data[i].news;
+        }
+    }
+}
+function n_no_of_news(n){
+    for(let i =0;i<n;i++){
+        console.log(data[i].news)
+    }
+}
+findbyid("b");
+n_no_of_news(2);
